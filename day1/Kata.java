2629cdc0fa1b9be1f02e23bbbe075f220bcde194
Bushra Fatima <bushrafatima@192.168.1.111>
2024-02-21 09:26:09 +0000

    Add day 1 challenge and solution

---
+++ day1/Kata.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            int negativeNum = number *= -1;
+            return negativeNum;
+        }
+    }
+
+/*
+Explanation 
+i wanted to create a new variable to store the changes required to the number variable
+the request was to make a positive number negative
+In maths multiplying a number with a negative number creates a negative number
+here its multiplying it by -1 so that the input value does  not change
+ */ 
---
+++ day1/challenge.md
@@ -0,0 +1,9 @@
+Opposite Number - https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
